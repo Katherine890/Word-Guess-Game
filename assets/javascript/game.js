fa2720c12f4c237131e4797a4f9d9cb2390e7eaf
KitKat <katherine_nguyen7@yahoo.com>
2018-10-06 01:44:41 -0700

    version 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,115 +1,129 @@
-const wordBank =   // array of Words
+// This hides the display.
+document.getElementById("startGame").style.cssText= "display: none";  
+document.getElementById("gameOver").style.cssText= "display: none";
+document.getElementById("roundWon").style.cssText= "display: none";
+
+// Allows game to start when pressing a key.
+document.body.onkeydown = function(e){
+    if(e.keyCode !=  2){
+        newGame();
+        
+    }
+};
+
+// Starts the game and displays text with press of a key.
+function newGame() {
+    document.getElementById("startGame").style.cssText = "display: block";
+    gameStart = document.getElementById("begin");
+    gameStart.textContent = "Good Luck!";
+}
+
+
+ const wordBank =   // array of Words
     [
-        "spongebob",
-        "patrick",
-        "squidward",
-        "mr.krabs",
-        "plankton",
-        "sandy"
+        ["S","P","O","N","G","E","B","O","B"],
+        ["P","A","T","R","I","C","K"],
+        ["S","Q","U","I","D","W","A","R","D"],
+        ["M","R","K","R","A","B","S"],
+        ["P","L","A","N","K","T","O","N"],
+        ["S","A","N","D","Y"]
     ];
 
-    const maxTries = 13;                      // This is the max number of guesses.
+    
 
-    let guessedLetters = [];                  //This stores wrong letters in the array.
-    let randNum = Math.floor(Math.random() * wordBank.length);   // This randomizes the wordBank. 
-    let chosenWord = wordBank[randNum];       // chosenWord will generate random word.
-    let currentWord = [];                     // This stores correct letters in the array.
-    let underScore = [];                      // This is the blank spot.
-    let remainingGuesses = [];                // This stores number of remaining guesses left.
-    let gameStart = true;                     // Tells if the game started.
-    let gameFinish = false;                   // Tells if the game ended.
-    let totalWins = [];                       // stores number of total wins.
+    let fail = 0;                                                      // This defines wrong letters.
+    let randNum = Math.floor((Math.random() * (wordBank.length-1)));   // This randomizes the words in the wordBank. 
+    let chosenWord = wordBank[randNum];                                // chosenWord will generate random word.
+    let chosenLetter = new Array(chosenWord.length);                   // This separates the letters in each word as individual strings.
+    let trialLeft = 10;                                                // This defines number of guesses remaining.
+    let wins = 0;                                                      // This defines number of wins.
 
-    let wins = 0;
-    let guessesLeft = 13;
+    let totalWins = document.getElementById("totalWins");              // displays number of wins.
 
-    //currentWord = underScore[chosenWord];
+    // This is local storage for the scores.
+    if (typeof storedScore !== 'undefined') {
+    totalWins.textContent = storedScore;
+    
+    }
+    
 
     console.log(chosenWord);
 
-    // Create variables that hold references to the places in the HTML where we want to display things.
-    guessedLetters = document.getElementById("guessedLetters");
-    currentWord = document.getElementById("underScore");
-    underScore = document.getElementById("underScore");
-    remainingGuesses = document.getElementById("remainingGuesses");
-    totalWins = document.getElementById("totalWins");
-    gameStart = document.getElementById("begin");
+    wrongLetter = document.getElementById("wrongLetterContainer");      // displays letters already guessed.
+    remainingGuesses = document.getElementById("remainingGuesses");     // displays number of remaining guesses.
+    gameStart = document.getElementById("begin");                       // displays start of game.
 
     console.log("begin");
-   
-   
-    // Resets game.
-    function resetGame() {
-        guessesLeft = maxTries;
-    }
-
-    // Assures that you press any key to start.
-    if(guessesLeft === maxTries)addEventListener('keydown', function(event) {
-        gameStart.textContent = "Good Luck!";
-    // Display wins, current word, etc.
-        totalWins.textContent = wins;
-        remainingGuesses.textContent = guessesLeft;
-        currentWord.textContent = underScore; 
-        guessedLetters.textContent = "";
-    })
-
-    // Clear out arrays.
-        guessedLetters = [];
-        underScore = [];
-
-    // Hide game over and Try Again images/text
-    document.getElementById("TryAgain").style.cssText= "display: none";
-    document.getElementById("game-over").style.cssText= "display: none";
-
-
-    // New Method for underscore replacement
-    //if(currentWord.textContent = underScore)addEventListener('keydown', function(event) {
-      //  underScore.textContent = indexOf(chosenWord);
-    //})
+    remainingGuesses.textContent = trialLeft;                           
 
     //This replaces underscores with the correct letters. 
-    //chosenWord.length = how many letters in the word.
-    let generateUnderscore = () => {
-      for(let i = 0; i < chosenWord.length; i++) {
-           underScore.push('_');
-        }
-        return underScore;
-        
+    //chosenLetter.length = how many letters in the word.
+    for (var i = 0; i < chosenLetter.length; i++){
+        chosenLetter[i] = "_ ";
     }
-      console.log(generateUnderscore());
+    function printchosenLetter(){                                       // Prints the letters in the underscores (guessField).
+        for (var i = 0; i < chosenLetter.length; i++){
+        let currentWord = document.getElementById("currentWord"); 
+        let letter = document.createTextNode(chosenLetter[i]);
+        currentWord.appendChild(letter);
+        }
+    }
 
-        document.addEventListener('keydown', (event) => {
-        underScore = document.getElementById("underScore").innerHTML; 
-        currentWord = underScore.replace(underScore, chosenWord.length);
-        document.getElementById("underScore").innerHTML = chosenWord;
-    })
+    // This checks if the the letter we guessed matches the letters in the word.
+    let matchLetter = function(){
+        let c = document.guessField; 
+        let b = c.elements["guessBox"]; 
+        let userLetter = b.value;                                      // the letter provided by the user
+        let correctLetter = "";
+        userLetter = userLetter.toUpperCase();
+        for (let i = 0; i < chosenWord.length; i++){
+            if(chosenWord[i] === userLetter){
+                chosenLetter[i] = userLetter + " ";
+                correctLetter = true;
+            }
+        b.value = "";
+        }
+    
+        let currentWord = document.getElementById("currentWord");     // This deletes the underscores (guessField) and replaces it with the letter.
+            currentWord.innerHTML=""; 
+            printchosenLetter();
 
-   // document.addEventListener('keydown', (event) => {
-      //  let keyword = String.fromCharCode(event.keyCode);
-       // if(chosenWord.indexOf(keyword) > -1) {
-        // add to current words array
-       // currentWord.push(keyword);
-        // replaces underscore with correct letter
-       // underScore[chosenWord.indexOf(keyword)] = keyword;
-       // if(underScore.join('') == chosenWord) {
-       //     wins++
-       // }
-       // console.log(underScore);
-       // console.log(keyword);
-       // guessedLetters.push(keyword); 
-   // }
-        //console.log(guessedLetters);
-        
-        
-   // });
 
+    // if a guessed letter is not in the word, the letter will be stored in the wrongLetters.
+    if(!correctLetter){
+        let wrongLetter = document.getElementById("wrongLetterContainer");
+        let letter = document.createTextNode(" " + userLetter);
+        wrongLetter.appendChild(letter);        
+        fail++;                                                      // number of wrong letters increase.
+        let trialLeft = 10 - fail;                                   // number of remaining guesses decrease.
+        remainingGuesses.textContent = trialLeft;
+    }
     
 
-    //if(currentWord === chosenWord.length) {
-        //totalWins++
-    //}
-    
-    // Update display on HTML page
-   
+    //checks if all letters have been found.
+    let gameFinish = true;
+    for (let i = 0; i < chosenLetter.length; i++){
+        if(chosenLetter[i] === "_ "){
+            gameFinish = false;
+        }
+    }if(gameFinish){
+        wins++;                                                     // If all correct letters are in field, wins increase.
+        localStorage.setItem("totalScore", wins);
+        let storedScore = localStorage.getItem("totalScore").length++;
+        totalWins.textContent = storedScore;
+        document.getElementById("roundWon").style.cssText= "display: block";
+        
+       }
+    // If you get ten wrong letters, you lose the game.
+    if(fail === 10){
+        document.getElementById("submissionArea").style.cssText= "display: none";
+        document.getElementById("gameOver").style.cssText= "display: block";
+    }
+}
 
-  
+// Restarts game and gives us a new random word.
+function startOver(){
+     window.location.reload(false);   
+}
+
+// wins stays the same amount instead of starting back to 0.
